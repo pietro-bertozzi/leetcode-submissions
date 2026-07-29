c7ba098066fb35b1cfc1966481f7290cade898c6
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-07-29 15:20:04 +0000

    Sync LeetCode submission Runtime - 41 ms (75.68%), Memory - 125.1 MB (83.53%)

---
+++ 2392-successful-pairs-of-spells-and-potions/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size();
+        int m = potions.size();
+        sort(potions.begin(), potions.end());
+        vector<int> result(n);
+        for (int i = 0; i < n; ++i) {
+            long long min_potion = (success + spells[i] - 1) / spells[i];
+            auto it = lower_bound(potions.begin(), potions.end(), min_potion);
+            result[i] = potions.end() - it;
+        }
+        return result;
+    }
+};
