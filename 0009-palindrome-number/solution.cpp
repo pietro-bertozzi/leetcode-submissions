6b70e9bb84da48c39ac1374a14c4df04f97616bd
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-08-02 19:48:46 +0000

    Sync LeetCode submission Runtime - 4 ms (30.94%), Memory - 8.6 MB (37.36%)

---
+++ 0009-palindrome-number/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        long reverse = 0, xcopy = x;
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+        return reverse == xcopy;
+    }
+};
