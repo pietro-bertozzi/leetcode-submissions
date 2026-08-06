b69bf99e6f4c04b40716c7d1934cde99a9d5577a
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-08-06 09:47:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (69.75%)

---
+++ 0874-backspace-string-compare/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        while (i >= 0 || j >= 0) {
+            int ss = 0, st = 0;
+            while (i >= 0) {
+                if (s[i] == '#') { ss++; i--; }
+                else if (ss > 0) { ss--; i--; }
+                else break;
+            }
+            while (j >= 0) {
+                if (t[j] == '#') { st++; j--; }
+                else if (st > 0) { st--; j--; }
+                else break;
+            }
+            if ((i >= 0 && j >= 0 && s[i] != t[j])
+                || (i >= 0) != (j >= 0)) return false;
+            i--;
+            j--;
+        }
+        return true;
+    }
+};
