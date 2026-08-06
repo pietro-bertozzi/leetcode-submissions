097183150349d51bc9d9fc4befffbaac9fd352af
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-08-06 08:49:37 +0000

    Sync LeetCode submission Runtime - 2 ms (81.17%), Memory - 12.9 MB (95.83%)

---
+++ 0022-generate-parentheses/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    void backtrack(int open, int close, int n, string& current, vector<string>& result) {
+        if (current.length() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+        if (open < n) {
+            current.push_back('(');
+            backtrack(open + 1, close, n, current, result);
+            current.pop_back();
+        }
+        if (close < open) {
+            current.push_back(')');
+            backtrack(open, close + 1, n, current, result);
+            current.pop_back();
+        }
+    }
+
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string current = "";
+        backtrack(0, 0, n, current, result);
+        return result;
+    }
+};
