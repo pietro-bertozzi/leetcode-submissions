00c2db2168d2ff338a0860617d06605bade897d1
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-08-07 09:05:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (44.70%)

--- 0374-guess-number-higher-or-lower/solution.cpp
+++ 0374-guess-number-higher-or-lower/solution.cpp
@@ -10,7 +10,7 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        int l = 1, r = n;
+        int l = 0, r = n;
         while (l <= r) {
             int m = l + (r - l) / 2;
             int response = guess(m);
