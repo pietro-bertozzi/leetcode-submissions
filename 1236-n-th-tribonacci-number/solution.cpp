ffff92efac33eb9eb04c0edb7145352aa5b87383
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-08-09 18:13:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (51.22%)

--- 1236-n-th-tribonacci-number/solution.cpp
+++ 1236-n-th-tribonacci-number/solution.cpp
@@ -3,12 +3,11 @@ public:
     int tribonacci(int n) {
         if (n < 2) return n;
         int one = 0, two = 1, three = 1;
-        n -= 2;
-        while (n--) {
-            int temp = one;
+        for (int i = 2; i < n; ++i) {
+            int next = three + two + one;
             one = two;
             two = three;
-            three = two + one + temp;
+            three = next;
         }
         return three;
     }
