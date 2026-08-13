9d8e7f75cf91aa33da56a14cfc687102eead89ab
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-08-13 10:23:58 +0000

    Sync LeetCode submission Runtime - 11 ms (98.77%), Memory - 9.1 MB (97.29%)

--- 1250-longest-common-subsequence/solution.cpp
+++ 1250-longest-common-subsequence/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        if (text1.size() < text2.size()) swap(text1, text2);
+        if (text1.size() < text2.size()) swap(text1, text2); 
         int n = text1.size(), m = text2.size();
         vector<int> dp(m + 1, 0);
         for (int i = 1; i <= n; ++i) {
