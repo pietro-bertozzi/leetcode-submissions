d5fe0c12afc9bbc21347921093e18ff4b4bfe98a
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-08-22 12:52:59 +0000

    Sync LeetCode submission Runtime - 3 ms (79.30%), Memory - 14 MB (94.90%)

---
+++ 0300-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> tails;
+        for (int n : nums) {
+            auto it = lower_bound(tails.begin(), tails.end(), n);
+            if (it == tails.end()) tails.push_back(n);
+            else *it = n;
+        }
+        return tails.size();
+    }
+};
+
