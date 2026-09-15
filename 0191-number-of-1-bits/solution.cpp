aa0e0e5c95d7b1bb511b06b7b3f971cbced01bcc
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-09-15 08:03:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (99.12%)

---
+++ 0191-number-of-1-bits/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int result = 0;
+        while (n) {
+            n &= (n - 1);
+            result++;
+        }
+        return result;
+    }
+};
