c42095bf1bb92f407340f50bb2197a7e8944f919
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-09-21 13:56:40 +0000

    Sync LeetCode submission Runtime - 16 ms (47.50%), Memory - 23.3 MB (83.70%)

---
+++ 1777-determine-if-two-strings-are-close/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.length() != word2.length()) return false;
+        vector<int> c1(26, 0);
+        vector<int> c2(26, 0);
+        for (char c : word1) c1[c - 'a']++;
+        for (char c : word2) c2[c - 'a']++;
+        for (int i = 0; i < 26; ++i) {
+            if ((c1[i] > 0 && !c2[i]) || (!c1[i] && c2[i] > 0)) {
+                return false;
+            }
+        }
+        sort(c1.begin(), c1.end());
+        sort(c2.begin(), c2.end());
+        return c1 == c2;
+    }
+};
